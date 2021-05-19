7d2a1674472c9c52f83c8a2ae4847f37236f7f42
Igor <sav4igor@yandex.ru>
2021-05-19 12:40:36 +0300

    Removed imports

--- src/main/java/com/github/cassette/CassetteApplication.java
+++ src/main/java/com/github/cassette/CassetteApplication.java
@@ -1,9 +1,7 @@
 package com.github.cassette;
 
-import com.github.cassette.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
